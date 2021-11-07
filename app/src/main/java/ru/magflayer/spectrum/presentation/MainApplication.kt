162d9298953377f86a7046f787cb0585942f0d50
Ilya Pavlik <magflayer@gmail.com>
2021-11-07 12:16:53 +0700

    Removed supporting unnecessary drawables vector compat feature

--- app/src/main/java/ru/magflayer/spectrum/presentation/MainApplication.kt
+++ app/src/main/java/ru/magflayer/spectrum/presentation/MainApplication.kt
@@ -1,15 +1,7 @@
 package ru.magflayer.spectrum.presentation
 
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication : MultiDexApplication() {
-
-    companion object {
-        init {
-            AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
-        }
-    }
-}
+class MainApplication : MultiDexApplication()
