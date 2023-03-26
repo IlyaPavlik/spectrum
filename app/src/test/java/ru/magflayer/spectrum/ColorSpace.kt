454c6d9222b8dc6ac02e7c3d98ed52342051b6bf
Ilya Pavlik <magflayer@gmail.com>
2023-03-26 15:56:01 +0700

    Fix tests

--- app/src/test/java/ru/magflayer/spectrum/ColorSpace.kt
+++ app/src/test/java/ru/magflayer/spectrum/ColorSpace.kt
@@ -1,16 +1,16 @@
 package ru.magflayer.spectrum
 
 class ColorSpace(
-        val color: Int = 0,
-        val rgb: RGB,
-        val cmyk: CMYK,
-        val ryb: RYB,
-        val hex: HEX,
-        val hsl: HSL,
-        val hsv: HSV,
-        val xyz: XYZ,
-        val cieLab: CIE_LAB,
-        val ncs: NCS
+    val color: Int = 0,
+    val rgb: RGB,
+    val cmyk: CMYK,
+    val ryb: RYB,
+    val hex: HEX,
+    val hsl: HSL,
+    val hsv: HSV,
+    val xyz: XYZ,
+    val cieLab: CIE_LAB,
+    val ncs: NCS,
 ) {
 
     enum class RGB(val rgb: IntArray) {
@@ -18,7 +18,7 @@ class ColorSpace(
         WHITE(intArrayOf(255, 255, 255)),
         RED(intArrayOf(255, 0, 0)),
         GREEN(intArrayOf(0, 255, 0)),
-        BLUE(intArrayOf(0, 0, 255));
+        BLUE(intArrayOf(0, 0, 255)),
     }
 
     enum class CMYK(val cmyk: IntArray) {
@@ -26,7 +26,7 @@ class ColorSpace(
         WHITE(intArrayOf(0, 0, 0, 0)),
         RED(intArrayOf(0, 100, 100, 0)),
         GREEN(intArrayOf(100, 0, 100, 0)),
-        BLUE(intArrayOf(100, 100, 0, 0));
+        BLUE(intArrayOf(100, 100, 0, 0)),
     }
 
     enum class RYB(val ryb: IntArray) {
@@ -34,7 +34,7 @@ class ColorSpace(
         WHITE(intArrayOf(100, 100, 100)),
         RED(intArrayOf(100, 0, 0)),
         GREEN(intArrayOf(0, 100, 100)),
-        BLUE(intArrayOf(0, 0, 100));
+        BLUE(intArrayOf(0, 0, 100)),
     }
 
     enum class HEX(val hex: String) {
@@ -42,7 +42,7 @@ class ColorSpace(
         WHITE("#FFFFFF"),
         RED("#FF0000"),
         GREEN("#00FF00"),
-        BLUE("#0000FF");
+        BLUE("#0000FF"),
     }
 
     enum class HSL(val hsl: FloatArray) {
@@ -50,7 +50,7 @@ class ColorSpace(
         WHITE(floatArrayOf(0f, 0f, 1f)),
         RED(floatArrayOf(0f, 1f, 0.5f)),
         GREEN(floatArrayOf(120f, 1f, 0.5f)),
-        BLUE(floatArrayOf(240f, 1f, 0.5f));
+        BLUE(floatArrayOf(240f, 1f, 0.5f)),
     }
 
     enum class HSV(val hsl: IntArray) {
@@ -58,7 +58,7 @@ class ColorSpace(
         WHITE(intArrayOf(0, 0, 100)),
         RED(intArrayOf(0, 100, 100)),
         GREEN(intArrayOf(120, 100, 100)),
-        BLUE(intArrayOf(240, 100, 100));
+        BLUE(intArrayOf(240, 100, 100)),
     }
 
     enum class XYZ(val xyz: DoubleArray) {
@@ -66,7 +66,7 @@ class ColorSpace(
         WHITE(doubleArrayOf(95.05, 100.0, 108.9)),
         RED(doubleArrayOf(41.24, 21.26, 1.93)),
         GREEN(doubleArrayOf(35.76, 71.52, 11.92)),
-        BLUE(doubleArrayOf(18.05, 7.22, 95.05));
+        BLUE(doubleArrayOf(18.05, 7.22, 95.05)),
     }
 
     enum class CIE_LAB(val cieLab: DoubleArray) {
@@ -74,7 +74,7 @@ class ColorSpace(
         WHITE(doubleArrayOf(100.0, 0.0053, -0.0104)),
         RED(doubleArrayOf(53.2329, 80.1093, 67.2201)),
         GREEN(doubleArrayOf(87.737, -86.1846, 83.1812)),
-        BLUE(doubleArrayOf(32.3026, 79.1967, -107.8637));
+        BLUE(doubleArrayOf(32.3026, 79.1967, -107.8637)),
     }
 
     enum class NCS(val code: String) {
@@ -82,6 +82,6 @@ class ColorSpace(
         WHITE("0300 N"),
         RED("0585 Y80R"),
         GREEN("0560 G20Y"),
-        BLUE("2060 R70B");
+        BLUE("2060 R70B"),
     }
 }
--- app/src/test/java/ru/magflayer/spectrum/ColorTest.kt
+++ app/src/test/java/ru/magflayer/spectrum/ColorTest.kt
@@ -10,21 +10,76 @@ class ColorTest {
 
     private val testColorList = object : ArrayList<ColorSpace>() {
         init {
-            add(ColorSpace(Color.RED, ColorSpace.RGB.RED, ColorSpace.CMYK.RED,
-                    ColorSpace.RYB.RED, ColorSpace.HEX.RED, ColorSpace.HSL.RED,
-                    ColorSpace.HSV.RED, ColorSpace.XYZ.RED, ColorSpace.CIE_LAB.RED, ColorSpace.NCS.RED))
-            add(ColorSpace(Color.GREEN, ColorSpace.RGB.GREEN, ColorSpace.CMYK.GREEN,
-                    ColorSpace.RYB.GREEN, ColorSpace.HEX.GREEN, ColorSpace.HSL.GREEN,
-                    ColorSpace.HSV.GREEN, ColorSpace.XYZ.GREEN, ColorSpace.CIE_LAB.GREEN, ColorSpace.NCS.GREEN))
-            add(ColorSpace(Color.BLUE, ColorSpace.RGB.BLUE, ColorSpace.CMYK.BLUE,
-                    ColorSpace.RYB.BLUE, ColorSpace.HEX.BLUE, ColorSpace.HSL.BLUE,
-                    ColorSpace.HSV.BLUE, ColorSpace.XYZ.BLUE, ColorSpace.CIE_LAB.BLUE, ColorSpace.NCS.BLUE))
-            add(ColorSpace(Color.BLACK, ColorSpace.RGB.BLACK, ColorSpace.CMYK.BLACK,
-                    ColorSpace.RYB.BLACK, ColorSpace.HEX.BLACK, ColorSpace.HSL.BLACK,
-                    ColorSpace.HSV.BLACK, ColorSpace.XYZ.BLACK, ColorSpace.CIE_LAB.BLACK, ColorSpace.NCS.BLACK))
-            add(ColorSpace(Color.WHITE, ColorSpace.RGB.WHITE, ColorSpace.CMYK.WHITE,
-                    ColorSpace.RYB.WHITE, ColorSpace.HEX.WHITE, ColorSpace.HSL.WHITE,
-                    ColorSpace.HSV.WHITE, ColorSpace.XYZ.WHITE, ColorSpace.CIE_LAB.WHITE, ColorSpace.NCS.WHITE))
+            add(
+                ColorSpace(
+                    Color.RED,
+                    ColorSpace.RGB.RED,
+                    ColorSpace.CMYK.RED,
+                    ColorSpace.RYB.RED,
+                    ColorSpace.HEX.RED,
+                    ColorSpace.HSL.RED,
+                    ColorSpace.HSV.RED,
+                    ColorSpace.XYZ.RED,
+                    ColorSpace.CIE_LAB.RED,
+                    ColorSpace.NCS.RED,
+                ),
+            )
+            add(
+                ColorSpace(
+                    Color.GREEN,
+                    ColorSpace.RGB.GREEN,
+                    ColorSpace.CMYK.GREEN,
+                    ColorSpace.RYB.GREEN,
+                    ColorSpace.HEX.GREEN,
+                    ColorSpace.HSL.GREEN,
+                    ColorSpace.HSV.GREEN,
+                    ColorSpace.XYZ.GREEN,
+                    ColorSpace.CIE_LAB.GREEN,
+                    ColorSpace.NCS.GREEN,
+                ),
+            )
+            add(
+                ColorSpace(
+                    Color.BLUE,
+                    ColorSpace.RGB.BLUE,
+                    ColorSpace.CMYK.BLUE,
+                    ColorSpace.RYB.BLUE,
+                    ColorSpace.HEX.BLUE,
+                    ColorSpace.HSL.BLUE,
+                    ColorSpace.HSV.BLUE,
+                    ColorSpace.XYZ.BLUE,
+                    ColorSpace.CIE_LAB.BLUE,
+                    ColorSpace.NCS.BLUE,
+                ),
+            )
+            add(
+                ColorSpace(
+                    Color.BLACK,
+                    ColorSpace.RGB.BLACK,
+                    ColorSpace.CMYK.BLACK,
+                    ColorSpace.RYB.BLACK,
+                    ColorSpace.HEX.BLACK,
+                    ColorSpace.HSL.BLACK,
+                    ColorSpace.HSV.BLACK,
+                    ColorSpace.XYZ.BLACK,
+                    ColorSpace.CIE_LAB.BLACK,
+                    ColorSpace.NCS.BLACK,
+                ),
+            )
+            add(
+                ColorSpace(
+                    Color.WHITE,
+                    ColorSpace.RGB.WHITE,
+                    ColorSpace.CMYK.WHITE,
+                    ColorSpace.RYB.WHITE,
+                    ColorSpace.HEX.WHITE,
+                    ColorSpace.HSL.WHITE,
+                    ColorSpace.HSV.WHITE,
+                    ColorSpace.XYZ.WHITE,
+                    ColorSpace.CIE_LAB.WHITE,
+                    ColorSpace.NCS.WHITE,
+                ),
+            )
         }
     }
 
@@ -33,7 +88,7 @@ class ColorTest {
     fun rgbTest() {
         for (colorSpace in testColorList) {
             val rgb = ColorHelper.dec2Rgb(colorSpace.color)
-            assertTrue(colorSpace.hex.toString(), Arrays.equals(colorSpace.rgb.rgb, rgb))
+            assertTrue(colorSpace.hex.toString(), colorSpace.rgb.rgb.contentEquals(rgb))
         }
     }
 
@@ -41,7 +96,7 @@ class ColorTest {
     fun cmykTest() {
         for (colorSpace in testColorList) {
             val cmyk = ColorHelper.dec2Cmyk(colorSpace.color)
-            assertTrue(colorSpace.hex.toString(), Arrays.equals(colorSpace.cmyk.cmyk, cmyk))
+            assertTrue(colorSpace.hex.toString(), colorSpace.cmyk.cmyk.contentEquals(cmyk))
         }
     }
 
@@ -49,7 +104,7 @@ class ColorTest {
     fun hslTest() {
         for (colorSpace in testColorList) {
             val hsl = ColorHelper.dec2Hsl(colorSpace.color)
-            assertTrue(colorSpace.hex.toString(), Arrays.equals(colorSpace.hsl.hsl, hsl))
+            assertTrue(colorSpace.hex.toString(), colorSpace.hsl.hsl.contentEquals(hsl))
         }
     }
 
@@ -57,7 +112,7 @@ class ColorTest {
     fun xyzTest() {
         for (colorSpace in testColorList) {
             val xyz = ColorHelper.dec2Xyz(colorSpace.color)
-            assertTrue(colorSpace.hex.toString(), Arrays.equals(colorSpace.xyz.xyz, xyz))
+            assertTrue(colorSpace.hex.toString(), colorSpace.xyz.xyz.contentEquals(xyz))
         }
     }
 
@@ -65,7 +120,7 @@ class ColorTest {
     fun labTest() {
         for (colorSpace in testColorList) {
             val ciaLab = ColorHelper.dec2Lab(colorSpace.color)
-            assertTrue(colorSpace.hex.toString(), Arrays.equals(colorSpace.cieLab.cieLab, ciaLab))
+            assertTrue(colorSpace.hex.toString(), colorSpace.cieLab.cieLab.contentEquals(ciaLab))
         }
     }
 
